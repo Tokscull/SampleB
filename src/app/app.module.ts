48e53f56d8567516a8481fa6382baf8f98936a11
Tokscull <egor.kirilenko.99@gmail.com>
2019-03-27 00:49:57 +0300

    import FormsModule & RouterModule & Component & Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {CarComponent} from './components/car/car.component';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {ColorService} from './serveces/color/color.service';
+
+const appRouter: Routes = [
+  {path: '', component: CarComponent},
+  {path: 'about', component: ContactsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRouter)
   ],
-  providers: [],
+  providers: [ColorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

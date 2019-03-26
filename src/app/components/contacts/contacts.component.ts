31d9899465471cd4e0a2a6f3c2d8bc26a0c450fc
Tokscull <egor.kirilenko.99@gmail.com>
2019-03-27 00:58:31 +0300

    add ngStyle

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  styleExp: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.styleExp = 'italic';
   }
 
 }
